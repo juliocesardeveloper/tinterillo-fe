6fad5d9be10202956e0281e210753e88ef4207d4
juliocesardeveloper <juliocesardeveloper@gmail.com>
2021-03-04 21:43:14 -0500

    fixes

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,54 +4,57 @@ import Swal from 'sweetalert2'
 
 
 
-export const startLogin = ( email, password ) => {
-
-  return async() => {
-
-    const resp = await fetchSinToken('auth/sign-in', { email, password }, 'POST');
-    const body = await resp.json();
-
-    console.log(body);
-
-  }
-
-
-}
-
-
-
-
 // export const startLogin = ( email, password ) => {
-//   return async( dispatch ) => {
-//     const resp = await fetchSinToken( 'auth/sign-in', { email, password }, 'POST' );
+
+//   return async() => {
+
+//     const resp = await fetchSinToken('auth/sign-in', { email, password }, 'POST');
 //     const body = await resp.json();
 
-//     if ( body.ok ) {
-//       localStorage.setItem( 'token', body.token );
-//       localStorage.setItem( 'token-init-date', new Date().getTime() );
+//     console.log(body.message);
 
-//       dispatch( login({
-//         uid: body.uid,
-//         name: body.name
-//       }))
-//     } else {
-//       Swal.fire( 'Error', body.msg, 'error' );
-//     }
 //   }
+
+
 // }
+
+
+
+
+export const startLogin = ( email, password ) => {
+  return async( dispatch ) => {
+    const resp = await fetchSinToken( 'auth/sign-in', { email, password }, 'POST' );
+    const body = await resp.json();
+
+    console.log(body.user);
+
+    if ( body.message === "This user exist." ) {
+      localStorage.setItem( 'token', body.token );
+      localStorage.setItem( 'token-init-date', new Date().getTime() );
+
+      dispatch( login({
+        uid: body.user._id,
+        name: body.user.name
+      }))
+    } else {
+      // Swal.fire( 'Error', body.msg, 'error' );
+      console.log(body.message);
+    }
+  }
+}
 
 export const startRegister = ( email, password, name ) => {
   return async( dispatch ) => {
     const resp = await fetchSinToken( 'auth/sign-up', { email, password, name }, 'POST' );
     const body = await resp.json();
 
-    if ( body.ok ) {
+    if ( body.message === "This user exist." ) {
       localStorage.setItem( 'token', body.token );
       localStorage.setItem( 'token-init-date', new Date().getTime() );
 
       dispatch( login({
-        uid: body.uid,
-        name: body.name
+        uid: body.user._id,
+        name: body.user.name
       }))
     } else {
       Swal.fire( 'Error', body.msg, 'error' );

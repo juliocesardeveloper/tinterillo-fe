badc8865d9a0da047a3c631cda08cfffbe55af30
Johan <johan-avila@outlook.com>
2021-03-11 20:32:53 -0600

    Repair search

--- src/pages/Mainsearch.js
+++ src/pages/Mainsearch.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import Results from "../components/Results";
+import Results from "../components/results";
 import Nav from "../components/Nav";
 import ResultContent from '../components/Result-content'
 import logo from "../Images/logo-tinterillo-light-color.png";
 import { FaSearch } from 'react-icons/fa';
+import initialState from '../initialState';
 // import Swal from 'sweetalert2';
 
 export default function MainSearch() {
@@ -13,51 +14,64 @@ export default function MainSearch() {
   const [data, setData] = useState('')
   const [change, setChange] = useState(false)
   const history = useHistory()
-  const [dataApi, setDataApi] = useState([])
+  const [dataApi, setDataApi] = useState({})
 
-  const URL = 'https://searcher-col.herokuapp.com/api/es/search?search=pluralista&index=constitucion'
+  // const URL = 'https://searcher-col.herokuapp.com/api/es/search?search=pluralista&index=constitucion'
+  const URL = 'http://localhost:4000/api/es/search'
 
-  useEffect(() => {
-    fetch(URL)
+  const GetDataApi = (serched)=>{
+    useEffect(() => {
+      fetch(`${URL}?search=${serched}&index=constitucion_politica_de_colombia`)
       .then(response => response.json())
-      .then(res => setDataApi(res))
-  }, [])
-
+      .then(res => {
+        res.body.hits.hits.length > 0 
+        ? setDataApi(res)
+        : setDataApi(initialState)
+      })
+      .catch(err => {
+        console.log('[ERROR]');
+      })
+    }, [])
+  }
+  
+  GetDataApi("Derechos")
+  
   //Muestra el primer componente que tiene las tarjetas de resultados de busqueda
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (data){
+    // Search keywords in API
+    if (data) {
       setStyle('header__all')
       setShow(true)
       // history.push('/search')
-    }else{
+    } else {
       console.log('Campo de busqueda vacia');
     }
   }
   //Asigna el valor del input al estado "data"
-  const handleChange = (event) =>{
+  const handleChange = (event) => {
     setData(event.target.value)
   }
   //Muestra el segundo componente con los resultados de la busqueda
-  const handleClickResults = ()=>{
+  const handleClickResults = () => {
     setChange(true)
   }
 
-  return(
+  return (
     <>
       <form className={style} onSubmit={handleSubmit}>
         <header>
-          <Nav/>
+          <Nav />
           <main>
-            <img src={logo} alt='Logo'/>
+            <img src={logo} alt='Logo' />
             <h1>TINTERILLO APP</h1>
             <div>
               <section className="section__input">
-              <label>
-                <input type="text" onChange={handleChange}/>
-              </label>
+                <label>
+                  <input type="text" onChange={handleChange} />
+                </label>
                 <button className='icon-search' type='submit'>
-                  <FaSearch/>
+                  <FaSearch />
                 </button>
               </section>
             </div>
@@ -66,13 +80,13 @@ export default function MainSearch() {
       </form>
       {
         change
-        ?
-          <ResultContent info={dataApi}/>
-        :
+          ?
+          <ResultContent info={dataApi} />
+          :
           show &&
           dataApi.body.hits.hits.map(info => {
-            return(
-              <Results key={info._id} click={handleClickResults} info={info}/>
+            return (
+              <Results key={info._id} click={handleClickResults} info={info} />
             )
           })
       }

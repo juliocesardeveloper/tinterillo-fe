122e11e4b97ed78a5a9988981df79d05ee29b92d
Julio César Arroyave Herrera <52511013+juliocesardeveloper@users.noreply.github.com>
2021-03-23 11:51:59 -0500

    select inputs and Validations on register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { startRegister, startRegisterWithEmailPasswordName } from '../actions/auth'
 import { useForm } from '../hooks/useForm'
@@ -6,10 +6,60 @@ import validator from 'validator'
 import { uiCloseModal, uiRemoveError, uiSetError } from '../actions/ui'
 import { BiErrorAlt } from 'react-icons/bi'
 
+const departmentOptions = [
+  "Amazonas",
+  "Antioquia",
+  "Arauca",
+  "Atlantico",
+  "Bolivar",
+  "Boyaca",
+  "Caldas",
+  "Caqueta",
+  "Casanare",
+  "Cauca",
+  "Cesar",
+  "Choco",
+  "Cordoba",
+  "Cundinamarca",
+  "Guainia",
+  "Guaviare",
+  "Huila",
+  "La Guajira",
+  "Magdalena",
+  "Meta",
+  "Nariño",
+  "Putumayo",
+  "Quindio",
+  "Risaralda",
+  "Santander",
+  "Sucre",
+  "Tolima",
+  "Vaupes",
+  "Vichada",
+  "San Andres y Providencia",
+  "Norte de Santander",
+  "Valle del Cauca"
+]
+
+const genderOptions = [
+  "Femenino",
+  "Masculino",
+  "Otro",
+  "Prefiero no decir"
+]
+
 export const Register = () => {
 
   const dispatch = useDispatch();
   const { msgError,  } = useSelector(state => state.ui);
+  
+
+  // const [selectedDepartmentValue, handleSelectedDepartmentValues] = useForm({
+  //   rDepartment: ''
+  // })
+
+  // const { rDepartment } = selectedDepartmentValue
+
 
   const [ formRegisterValues, handleRegisterInputChange ] = useForm({
     rName: 'Johan',
@@ -45,7 +95,19 @@ export const Register = () => {
      } else if ( rPassword1 !== rPassword2 || rPassword1 < 5 ) {
        dispatch( uiSetError('El password debe tener al menos 6 caracteres y coincidir con el password de confirmación') );
        return false
-     }
+     } else if ( rDepartment === "" ) {
+      dispatch( uiSetError('Por favor escoje un departamento de residencia') );
+      return false
+    } else if ( rGender === "" ) {
+      dispatch( uiSetError('Por favor escoje una de las opciones en Género') );
+      return false
+    } else if ( rBirthday === "" ) {
+      dispatch( uiSetError('Por favor selecciona tu fecha de nacimiento') );
+      return false
+    } else if ( rJob === "" ) {
+      dispatch( uiSetError('Por favor escribe tu profesión o a qué te dedicas') );
+      return false
+    }
      dispatch( uiRemoveError() );
      return true;
    }
@@ -104,25 +166,34 @@ export const Register = () => {
             value={ rPassword2 }
             onChange={ handleRegisterInputChange }
           />
-          <input
-            type="text"
-            placeholder="Departamento de residencia"
-            name="rDepartment"
-            value={ rDepartment }
-            onChange={ handleRegisterInputChange }
-          />
-          <input
-            type="text"
-            placeholder="Género"
-            name="rGender"
-            value={ rGender }
-            onChange={ handleRegisterInputChange }
-          />
+          <select className="select" id="departments" name="rDepartment" value={rDepartment} onChange={handleRegisterInputChange}>
+            <option value="">Departamento</option>
+            {
+              departmentOptions.map((item, index) => (
+                <option className="option" key={ index }>{ item }</option>
+              ))
+            }
+          </select>
+          <select className="select" id="gender" name="rGender" value={rGender} onChange={handleRegisterInputChange}>
+            <option value="">Género</option>
+            {
+              genderOptions.map((item, index) => (
+                <option className="option" key={ index }>{ item }</option>
+              ))
+            }
+          </select>
           <input
             type="date"
             placeholder="Fecha de nacimiento"
             name="rBirthday"
             value={ rBirthday }
+            onChange={ handleRegisterInputChange }
+          />
+          <input
+            type="text"
+            placeholder="Profesión"
+            name="rJob"
+            value={ rJob }
             onChange={ handleRegisterInputChange }
           />
 
